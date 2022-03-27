fa4fcad6f7f6944bf3c9f6e484ca4946887133a1
lnicepei <dmitrii.shaidorov@gmail.com>
2022-03-27 08:49:20 +0300

    Backup just in case

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-alert("Guess the word!");
+getWord();
+function playGame(word) {
+  alert("Guess the word!");
+  let guess = "_".repeat(word.length);
 
-function playGame(word){
-    let guess = "_".repeat(word.length);
-    
-    for (let i = 0; i < 3; i++) {
-        let letter = prompt("Enter a letter");
-        if (word.includes(letter)) {
-            alert("There is letter " + letter);
-            let tempGuess = guess.split(""); //separates string into an array of letters (tempGuess) 
-            tempGuess[word.indexOf(letter)] = letter;
-            guess = tempGuess.join(""); 
-            console.log(guess);
+  for (let i = 0; i < 3; i++) {
+    let letter = prompt("Enter a letter");
+    let correctLetterArray = [];
+    if (word.includes(letter)) {
+      for (let f = 0; f < word.length; f++) {
+        if (word[f] == letter) {
+          correctLetterArray.push(f);
         }
+      }
+      console.log(correctLetterArray);
+      alert("There is letter " + letter);
+      let tempGuess = guess.split(""); //separates string into an array of letters (tempGuess)
+      for (k = 0; k < correctLetterArray.length - 1; k++) {
+          
+      }
+      tempGuess[word.indexOf(letter)] = letter;
+      guess = tempGuess.join("");
+      console.log(guess);
     }
+  }
 }
-function getWord(){
-    let word = prompt("Enter your word");
-    playGame(word);
+function getWord() {
+  let word = prompt("Enter your word");
+  playGame(word);
 }
